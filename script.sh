66d13fce905c3000cbfb039bb977a20756f88a03
anthony-marais <67336192+anthony-marais@users.noreply.github.com>
2023-01-07 16:54:02 +0100

    new file:   script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## mise a jours de la VM
+sudo apt-get update && apt-get ugrade -y
+## installation de docker 
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update && apt install docker-ce
+sudo usermod -aG docker ${USER}
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
